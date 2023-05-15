853b341288507cb2380f858aebc0fb95427d7a58
Sevda <119746347+sevdakeskin@users.noreply.github.com>
2023-05-15 14:33:42 +0300

    Add files via upload

---
+++ 1_Helloworld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+// solidity compiler versiyonu 0.8.17 ye eşit veya ondan büyük olmalıdır.
+// SPDX Licence -> Software Package Data Exchange
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    string  public greet = "Hello World! -sevda";
+}
